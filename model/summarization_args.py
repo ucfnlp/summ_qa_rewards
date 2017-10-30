2dfb73a9c49d549651f8368f2b946a8ec60388da
Kristjan Arumae <kristjan.arumae@gmail.com>
2017-10-29 20:07:48 -0400

    Updates for Cluster

--- model/summarization_args.py
+++ model/summarization_args.py
@@ -86,7 +86,7 @@ def get_args():
                         )
     parser.add_argument("--max_epochs",
                         type=int,
-                        default=10,
+                        default=30,
                         help="maximum # of epochs"
                         )
     parser.add_argument("--eval_period",
