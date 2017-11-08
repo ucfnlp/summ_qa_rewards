e3048f5e07652d007f33a31dbc9c9713654cc220
Kristjan Arumae <kristjan.arumae@gmail.com>
2017-11-08 12:05:59 -0500

    Edit:a number

--- model/summarization_args.py
+++ model/summarization_args.py
@@ -40,7 +40,7 @@ def get_args():
 
     parser.add_argument('--max_sentences_hl',
                         type=int,
-                        default=3,
+                        default=4,
                         help='Number of total sentences for highlights')
 
     parser.add_argument("--train_output_readable",
