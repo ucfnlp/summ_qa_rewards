6d6ca84c79b7d071381933e0ac7e714d8ad46a38
Kristjan Arumae <kristjan.arumae@gmail.com>
2017-11-28 01:24:30 -0500

    Word count update, multithreading fixes

--- model/myio.py
+++ model/myio.py
@@ -129,7 +129,7 @@ def write_summ_for_rouge(args, bz, bx, by, emb_layer):
     s_num = 1
     for z in xrange(len(bx)):
         for i in xrange(len(bx[z][0])):
-            ofp = open(args.system_summ_path + 's.' + str(s_num) + '.txt', 'w+')
+            ofp = open(args.system_summ_path + str(args.sparcity) + 's.' + str(s_num) + '.txt', 'w+')
 
             for j in xrange(len(bx[z])):
                 word = emb_layer.lst_words[bx[z][j][i]]
@@ -145,7 +145,7 @@ def write_summ_for_rouge(args, bz, bx, by, emb_layer):
     s_num = 1
     for batch in by:
         for i in xrange(len(batch[0])):
-            ofp = open(args.model_summ_path + 's.' + str(s_num) + '.txt', 'w+')
+            ofp = open(args.model_summ_path + str(args.sparcity) + 's.' + str(s_num) + '.txt', 'w+')
 
             for j in xrange(len(batch)):
                 word = emb_layer.lst_words[batch[j][i]]
@@ -163,8 +163,8 @@ def get_rouge(args):
     r = Rouge155()
     r.system_dir = args.system_summ_path
     r.model_dir = args.model_summ_path
-    r.system_filename_pattern = 's.(\d+).txt'
-    r.model_filename_pattern = 's.#ID#.txt'
+    r.system_filename_pattern = str(args.sparcity) + 's.(\d+).txt'
+    r.model_filename_pattern = str(args.sparcity) + 's.#ID#.txt'
 
     return r.convert_and_evaluate()
 
