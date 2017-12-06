bdf0cc3ad0b5114b4caf0efc16e4107c59b101a4
Kristjan Arumae <kristjan.arumae@gmail.com>
2017-12-06 01:23:18 -0500

    More weight analysis

--- nn/weight_analysis.py
+++ nn/weight_analysis.py
@@ -10,6 +10,7 @@ import summarization_args
 
 def weight_analysis(args):
     norms = []
+    weights = []
 
     for e in xrange(args.max_epochs):
         for i in xrange(args.batch):
@@ -19,13 +20,24 @@ def weight_analysis(args):
             values = []
             for item in a:
                 values.extend(item.ravel().tolist())
+
+            weights.append(values)
             norms.append(np.linalg.norm(values, ord=1))
 
             f.close()
 
+        plt.hist(weights[-1])
+        plt.ylabel('W')
+        plt.savefig('../data/results/plots/e_' + str(e+1) + '.png')
+        plt.clf()
+        plt.close()
+
     x = range(0,len(norms))
+    plt.ylabel('L1 Norm')
+    plt.xlabel('Batches')
     plt.plot(x, norms)
-    plt.show()
+    plt.savefig('../data/results/plots/norms_' + str(e) + '.png')
+    plt.close()
 
 
 if __name__ == "__main__":
