ebedc582d0eea20f3ba66b1e30f7f6bea62f8a41
kristjanarumae <kristjan.arumae@gmail.com>
2018-02-03 23:35:31 -0500

    Tracking % unused entities

--- data/process_data.py
+++ data/process_data.py
@@ -121,7 +121,8 @@ def prepare_l3(args, test):
         ofp = open(file_part + str(rouge_counter).zfill(6) + '.txt', 'w+')
 
         for i in xrange(3):
-
+            if i > len(article):
+                break
             text = ' '.join(article[i])
 
             ofp.write(text)
