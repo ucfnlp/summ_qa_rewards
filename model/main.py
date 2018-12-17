be59e44c339d679b3a4305f0f82cb5f4e9b26a42
karmaChamelion <kristjan.arumae@gmail.com>
2018-12-16 20:31:41 -0500

    Bug fixes for previous commit

--- model/main.py
+++ model/main.py
@@ -49,6 +49,8 @@ class Model(object):
         if args.qa_performance:
             self.encoder.ready_qa()
             self.params = self.encoder.params
+            self.generator.params = []
+
             for l in self.generator.layers + [self.embedding_layer] + [self.embedding_layer_posit]:
                 for p in l.params:
                     self.generator.params.append(p)
